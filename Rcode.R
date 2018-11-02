f8c9bcc3bc9e4915edae810ccc3cfbaf1dd85381
Eva-McLoughlin <mclougev@tcd.ie>
2018-11-02 17:18:43 +0000

    Adding files via upload

--- Rcode.R
+++ Rcode.R
@@ -20,3 +20,14 @@ gtoken <- config(token = github_token)
 req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
 
 # lines 8-20 sourced from: https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
+
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 

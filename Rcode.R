fefe78b870dffa9464bb9a2d324923ddca2b7012
Eva-McLoughlin <mclougev@tcd.ie>
2018-11-02 17:15:52 +0000

    Accessing github

--- Rcode.R
+++ Rcode.R
@@ -4,3 +4,19 @@ library(jsonlite)
 library(httpuv)
 #install.packages("httr")
 library(httr)
+
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "API_request_assignment",
+                   key = "8effa90261308b04b02a",
+                   secret = "3471fe07abad672dcdee77d293a38aa353962a25")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# lines 8-20 sourced from: https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08

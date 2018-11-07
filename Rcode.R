1810baec4255c934918432b0046b7e9a26e3e027
Eva-McLoughlin <mclougev@tcd.ie>
2018-11-07 19:08:13 +0000

    Added code to try pulling data about my own github account - working well. Should work for any github account

--- Rcode.R
+++ Rcode.R
@@ -17,21 +17,24 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API
 gtoken <- config(token = github_token)
-phadejReq <- GET("https://github.com/phadej/github", gtoken)
-phadejReq
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+req
 
 # lines 8-20 sourced from: https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
-stop_for_status(phadejReq)
-
-install.packages("xml2")
+stop_for_status(req)
 
 # Extract content from a request
-json1 = content(phadejReq)
+json1 = content(req)
 json1
 
 # Convert to a data.frame
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
+
+
+data <- fromJSON("https://api.github.com/users/Eva-McLoughlin")
+data$followers
+data$public_repos #number of public repositories i have

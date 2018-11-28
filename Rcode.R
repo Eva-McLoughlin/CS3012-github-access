5f2fca984af80718d5409a1b6bf0204f74ce35a0
Eva-McLoughlin <mclougev@tcd.ie>
2018-11-28 10:31:45 +0000

    Created the data frame that will be used for generating the graphs

--- Rcode.R
+++ Rcode.R
@@ -1,13 +1,14 @@
-install.packages("jsonlite")
+#install.packages("jsonlite")
 library(jsonlite)
-install.packages("httpuv")
+#install.packages("httpuv")
 library(httpuv)
-install.packages("httr")
+#install.packages("httr")
 library(httr)
+
 
 oauth_endpoints("github")
 
-# Change based on what you 
+# Change based on application
 myapp <- oauth_app(appname = "API_request_assignment",
                    key = "8effa90261308b04b02a",
                    secret = "3471fe07abad672dcdee77d293a38aa353962a25")
@@ -18,7 +19,6 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 # Use API
 gtoken <- config(token = github_token)
 req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
-req
 
 # lines 8-20 sourced from: https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
@@ -26,7 +26,6 @@ stop_for_status(req)
 
 # Extract content from a request
 json1 = content(req)
-json1
 
 # Convert to a data.frame
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
@@ -34,33 +33,12 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 # Subset data.frame
 gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 
+#-------------------------------------------------------------------------------------------------
 
 data <- fromJSON("https://api.github.com/users/Eva-McLoughlin")
 data$followers
 data$public_repos #number of public repositories i have
 
-data$id
-data$url
-data$followers_url
-data$following_url
-data$name
-data$company
-data$following
-data$name
-data$name
-data$created_at
-data$updated_at
-data$bio
-data$email
-data$type
-data$site_admin
-data$public_gists
-data$public_repos
-data$hireable
-data$login
-data$blog
-data$location
-data$repos_url
 
 #Information on repos
 repositories <- fromJSON("https://api.github.com/users/Eva-McLoughlin/repos")
@@ -69,9 +47,60 @@ repositories$created_at # when various repos were created
 ass1repos <- fromJSON("https://api.github.com/repos/Eva-McLoughlin/software-engineering/commits")
 ass1repos$commit$message #display the messages attached to each commit for this repos
 
-
 # information on followers 
 followers <- fromJSON("https://api.github.com/users/Eva-McLoughlin/followers")
 followers$login #usernames of all followers
 length <- length(followers$login) #number of people who follow me
 length
+
+# view this data as JSON
+dataJSon = toJSON(data, pretty = TRUE)
+dataJSon
+
+# Seeing as I have no followers, my data is not very interesting.
+# Want to interrogtate a user with more interesting data -> turanct (selected randomly from github)
+
+dataT <- fromJSON("https://api.github.com/users/turanct")
+dataT$followers
+dataT$public_repos
+
+#---------------------------------------------------------------------------------------------------
+
+# PROCESSING THE DATA FROM GITHUB
+
+#I am going to gather information about the number of followers cassidke's followers have
+
+followersNames <- fromJSON("https://api.github.com/users/cassidke/followers")
+followersNames$login #shown previously, gets the user names of my followers
+
+a <- "https://api.github.com/users/"
+b <- followersNames$login[4]
+b
+c <- "/followers"
+
+test <- sprintf("%s%s%s", a,b,c) #this method amalgamates a, b and c into one string 
+test                              #called test 
+
+
+#I now have access to joostmarkerink's followers
+#If I wanted access to karel1980's followers, I woiuld change b to b <- followersNames$login[2] 
+#as she is second in the list.
+
+#Step 2:
+
+numberOfFollowers <- c() #this creates an empty vector
+namesOfFollowers <- c()
+for (i in 1:length(followersNames$login)) {
+  followers <- followersNames$login[i] #loops through each of my followers, indexed by i
+  jsonLink <- sprintf("%s%s%s", a, followers, c) #creates link for my ith follower
+  followData <- fromJSON(jsonLink) #stores the followers of my ith follower
+  numberOfFollowers[i] = length(followData$login) #amount of followers the ith follower has
+  namesOfFollowers[i] = followers #names of all users following the ith follower
+  
+}
+numberOfFollowers
+namesOfFollowers
+finalData <- data.frame(numberOfFollowers, namesOfFollowers) #stores two vectors as one
+#data frame
+finalData$namesOfFollowers
+finalData$numberOfFollowers
